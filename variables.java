6487817d99029d9df82b73fc8d80f50cc3a00ef2
Katie Mangan <katie.mangan1@gmail.com>
2019-05-28 21:23:00 -0500

    create class to practice variables and print to terminal

---
+++ variables.java
@@ -0,0 +1,10 @@
+public class variables {
+    public static void main(String[] args) {
+        String firstName = "Katie";
+        String lastName = "Mangan";
+        boolean hungry = false;
+        char firstInitial = 'K';
+        double GPA = 4.0;
+        System.out.println(firstName + lastName + hungry + firstInitial + GPA);
+    }
+}
